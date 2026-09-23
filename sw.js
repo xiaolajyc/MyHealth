e94835ac9ac61d555d1193ce7201a08e36d8e2bc
xiaolajyc <72300443+xiaolajyc@users.noreply.github.com>
2026-09-23 14:54:21 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION="2026-09-23-v3.1";
+const VERSION="2026-09-23-v3.2";
 const CACHE=`my-health-${VERSION}`;
 const SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon-180.png"];
 
